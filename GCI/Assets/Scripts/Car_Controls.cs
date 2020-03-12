649f653e79a729d71a5f4d60c3a7b5b8aff4450f
Arshia <arshiasharma.4@gmail.com>
2020-03-12 00:34:12 +0000

    Arrow keys and endless roads

---
+++ GCI/Assets/Scripts/Car_Controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Controls : MonoBehaviour
+{
+
+    public float carSpeed;
+    Vector3 position;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+
+        transform.position = position;
+
+    }
+
+
+}
